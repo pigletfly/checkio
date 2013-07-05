8b8d8b0b10ba5bf27df8f8563127158bb90fdae7
pigletfly <wangbing.adam@gmail.com>
2013-07-05 09:07:31 +0800

    delete

--- task2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import re
-def checkio(data):
-	pattern = re.compile('\s+')
-	return re.sub(pattern,' ',data) 
-
-if __name__ == '__main__':
-	print checkio('I  like       python')
